f9add2b5388e3c80c0d311020111ad694e014eb2
Henry Ballinger <henrybmcf@gmail.com>
2016-02-23 19:53:26 +0000

    Beginning keyword/phrase match search

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -7,17 +7,37 @@ import java.util.Random;
 
 public class Chatbot {
     public static String uInput;
-    public static String bOutput;
+    public static String bOutput = "";
+    public static boolean respond;
     public static boolean exit;
 
-    static String[] knowledge = {
-            "I heard you!",
-            "So, you are talking to me.",
-            "Continue, I'm listening",
-            "Very interesting conversation.",
-            "Please, tell me more.."
+    // First line is the key word/phrase
+    // The rest are the responses
+    static String[][] knowledge = {
+            {
+                    "what is your name",
+                    "what would you like my name to be?",
+                    "why do you want to know?",
+                    "you can call me Jarvis",
+            },
+
+            {
+                    "hello",
+                    "hello.",
+                    "hi, how are you?",
+                    "nice to meet you",
+            },
+
+            {
+                    "I heard you!",
+                    "So, you are talking to me.",
+                    "Continue, I'm listening",
+                    "Very interesting conversation.",
+                    "Please, tell me more.."
+            },
     };
 
+    // List of possible user inputs to end the conversation
     static String[] goodbye = {
             "bye",
             "goodbye",
@@ -47,9 +67,22 @@ public class Chatbot {
                 break;
             }
             else {
+                int j;
+                for (j = 0; j < knowledge.length - 1; j++) {
+                    if (Levenshtein.MinimumEditDistance(uInput, knowledge[j][0]) < 3) {
+                        respond = true;
+                        break;
+                    }
+                }
+
+                int response;
                 Random rand = new Random();
-                int response = rand.nextInt(3);
-                bOutput = knowledge[response];
+                if (respond)
+                    response = rand.nextInt(knowledge[j].length - 1) + 1;
+                else
+                    response = rand.nextInt(knowledge[knowledge.length - 1].length);
+                bOutput = knowledge[j][response];
+                bOutput = bOutput.substring(0, 1).toUpperCase() + bOutput.substring(1);
                 System.out.println(bOutput);
             }
         }
