02dafa8e82ab620b54cc070dcdc8216cf5412d79
Henry Ballinger <henrybmcf@gmail.com>
2016-02-23 20:27:48 +0000

    Multiple keywords per set of responses. Needs tidying.

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -7,33 +7,47 @@ import java.util.Random;
 
 public class Chatbot {
     public static String uInput;
-    public static String bOutput = "";
+    public static String bOutput;
     public static boolean respond;
     public static boolean exit;
 
     // First line is the key word/phrase
     // The rest are the responses
-    static String[][] knowledge = {
+    static String[][][] knowledge = {
             {
-                    "what is your name",
-                    "what would you like my name to be?",
-                    "why do you want to know?",
-                    "you can call me Jarvis",
+                    {
+                            "what is your name",
+                            "what's your name",
+                            "your name"
+                    },
+                    {
+                            "what would you like my name to be?",
+                            "why do you want to know?",
+                            "you can call me Jarvis",
+                    }
             },
 
             {
-                    "hello",
-                    "hello.",
-                    "hi, how are you?",
-                    "nice to meet you",
+                    {
+                            "hello",
+                            "hi"
+                    },
+                    {
+                            "hello.",
+                            "hi, how are you?",
+                            "nice to meet you",
+                    }
             },
 
+            // Default responses for when no keyword matches
             {
-                    "I heard you!",
-                    "So, you are talking to me.",
-                    "Continue, I'm listening",
-                    "Very interesting conversation.",
-                    "Please, tell me more.."
+                    {
+                            "I heard you!",
+                            "So, you are talking to me.",
+                            "Continue, I'm listening",
+                            "Very interesting conversation.",
+                            "Please, tell me more.."
+                    }
             },
     };
 
@@ -42,11 +56,13 @@ public class Chatbot {
             "bye",
             "goodbye",
             "see you later",
-            "i have to go"
+            "i have to go",
+            "see ya"
     };
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+
         do {
             System.out.print("> ");
             Scanner scanner = new Scanner(System.in);
@@ -67,25 +83,34 @@ public class Chatbot {
                 break;
             }
             else {
-                int j;
+                int j, k;
+                search:
                 for (j = 0; j < knowledge.length - 1; j++) {
-                    if (Levenshtein.MinimumEditDistance(uInput, knowledge[j][0]) < 3) {
-                        respond = true;
-                        break;
+                    for (k = 0; k < knowledge[j][0].length; k++) {
+                        if (Levenshtein.MinimumEditDistance(uInput, knowledge[j][0][k]) < 3) {
+                            respond = true;
+                            break search;
+                        }
                     }
                 }
 
                 int response;
                 Random rand = new Random();
                 if (respond)
-                    response = rand.nextInt(knowledge[j].length - 1) + 1;
+                    response = rand.nextInt(knowledge[j][1].length);
                 else
                     response = rand.nextInt(knowledge[knowledge.length - 1].length);
-                bOutput = knowledge[j][response];
-                bOutput = bOutput.substring(0, 1).toUpperCase() + bOutput.substring(1);
+                bOutput = knowledge[j][1][response];
+                bOutput = InitCap(bOutput);
                 System.out.println(bOutput);
             }
         }
         while (true);
+    }
+
+    // Capitalise first letter of string
+    public static String InitCap(String str) {
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+        return str;
     }
 }
